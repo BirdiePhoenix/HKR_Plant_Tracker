9b38cde8755956bcfaf9c23fe1a1553d9fc36cb6
Felix <felix.william1993@gmail.com>
2026-04-08 14:37:42 +0200

    Created classes for Plant and WateringLog with constructors

---
+++ Plant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKR_Plant_Tracker
+{
+    internal class Plant
+    {
+        private string plantID;
+        private string plantName;
+        private string plantLocation;
+        private int wateringDays;
+        public Plant(string PlantID, string PlantName, string PlantLocation, int WateringDays) {
+            this.plantID = PlantID;
+            this.plantName = PlantName;
+            this.plantLocation = PlantLocation;
+            this.wateringDays = WateringDays;
+        }
+    }
+}
---
+++ WateringLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKR_Plant_Tracker
+{
+    internal class WateringLog
+    {
+        private string plantID;
+        private DateTime logDate;
+        private string logNotes;
+        public WateringLog(string PlantID, DateTime LogDate, string LogNotes) {
+            this.plantID = PlantID;
+            this.logDate = LogDate;
+            this.logNotes = LogNotes;
+        }
+    }
+}
