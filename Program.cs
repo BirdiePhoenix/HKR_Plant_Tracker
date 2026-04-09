d63b2d9792f38af1d1574bf18537d517a68c711b
Felix <felix.william1993@gmail.com>
2026-04-09 12:24:15 +0200

    Created Main menu

--- Program.cs
+++ Program.cs
@@ -6,9 +6,37 @@ namespace HKR_Plant_Tracker
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Plant Tracker \n **********");
+            string menuChoice;
+            Console.WriteLine("Plant Tracker \n **********\n");
 
+            do
+            {
+                Console.WriteLine("1.Plant Menu\n2.Log Menu\n9. Exit");
 
+                menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        PlantMenu();
+                        break;
+                    case "2":
+                        LogMenu();
+                        break;
+                    case "9":
+                        break;
+                }
+            } while (menuChoice != "9");
+        }
+
+        static void PlantMenu()
+        {
+            Console.WriteLine("Plant Menu");
+        }
+
+        static void LogMenu()
+        {
+            Console.WriteLine("Log Menu");
         }
     }
 }
