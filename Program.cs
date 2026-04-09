0083efd092f26f96a87eacc2f768bade70ccf160
Felix <felix.william1993@gmail.com>
2026-04-09 12:36:54 +0200

    Added PlantMenu and LogMenu

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ namespace HKR_Plant_Tracker
         private static void Main(string[] args)
         {
             string menuChoice;
-            Console.WriteLine("Plant Tracker \n **********\n");
+            Console.WriteLine("PLANT TRACKER \n **********\n");
 
             do
             {
-                Console.WriteLine("1.Plant Menu\n2.Log Menu\n9. Exit");
+                Console.WriteLine("1.Plant Menu\n2.Log Menu\n9. Exit\n");
 
                 menuChoice = Console.ReadLine();
 
@@ -31,12 +31,61 @@ namespace HKR_Plant_Tracker
 
         static void PlantMenu()
         {
-            Console.WriteLine("Plant Menu");
+            string menuChoice;
+            Console.WriteLine("PLANT MENU\n");
+
+            do
+            {
+                Console.WriteLine("1.Add new plant\n2.View all plants\n3.Delete plant\n4.Search plant\n9.Back to main menu\n");
+
+                menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        Console.WriteLine("Add plant\n");
+                        break;
+                    case "2":
+                        Console.WriteLine("View plants\n");
+                        break;
+                    case "3":
+                        Console.WriteLine("Delete plant\n");
+                        break;
+                    case "4":
+                        Console.WriteLine("Search plant\n");
+                        break;
+                    case "9":
+                        break;
+                }
+            } while (menuChoice != "9");
         }
 
         static void LogMenu()
         {
-            Console.WriteLine("Log Menu");
+            string menuChoice;
+            Console.WriteLine("LOG MENU\n");
+
+            do
+            {
+                Console.WriteLine("1.Log watering\n2.View all logs\n3.View log for plant\n9.Back to main menu\n");
+
+                menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        Console.WriteLine("Watering\n");
+                        break;
+                    case "2":
+                        Console.WriteLine("Viewing all logs\n");
+                        break;
+                    case "3":
+                        Console.WriteLine("Viewing plant log\n");
+                        break;
+                    case "9":
+                        break;
+                }
+            } while (menuChoice != "9");
         }
     }
 }
